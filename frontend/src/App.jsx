be1ff95636c5a1ae31de72867dfd175a1e968e8e
0xward <aradeawardana97@gmail.com>
2025-10-01 21:06:59 +0700

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import PoolList from "./components/PoolList";
+
+function App() {
+  return (
+    <div className="p-6 font-sans">
+      <h1 className="text-2xl font-bold mb-4">📈 DeFi Yield Calculator</h1>
+      <PoolList />
+    </div>
+  );
+}
+
+export default App;
